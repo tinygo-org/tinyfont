4d565c790a1cc5e0e34e43c28bf3ad4c40b95a08
sago35 <sago35@gmail.com>
2020-03-13 00:46:34 +0900

    Change []byte to []rune

--- tinyfont.go
+++ tinyfont.go
@@ -12,6 +12,7 @@ const (
 	ROTATION_180 Rotation = 2
 	ROTATION_270 Rotation = 3
 )
+
 type Rotation uint8
 
 type Glyph struct {
@@ -32,16 +33,16 @@ type Font struct {
 }
 
 // DrawChar sets a single char in the buffer of the display
-func DrawChar(display drivers.Displayer, font *Font, x int16, y int16, char byte, color color.RGBA) {
+func DrawChar(display drivers.Displayer, font *Font, x int16, y int16, char rune, color color.RGBA) {
 	DrawCharRotated(display, font, x, y, char, color, 0)
 }
 
 // DrawCharRotated sets a single char in the buffer of the display
-func DrawCharRotated(display drivers.Displayer, font *Font, x int16, y int16, char byte, color color.RGBA, rotation Rotation) {
-	if char < font.First || char > font.Last {
+func DrawCharRotated(display drivers.Displayer, font *Font, x int16, y int16, char rune, color color.RGBA, rotation Rotation) {
+	if byte(char) < font.First || byte(char) > font.Last {
 		return
 	}
-	glyph := font.Glyphs[char-font.First]
+	glyph := font.Glyphs[byte(char)-font.First]
 	bitmapOffset := glyph.BitmapIndex
 	bitmap := font.Bitmaps[bitmapOffset]
 	bit := uint8(0)
@@ -72,17 +73,17 @@ func DrawCharRotated(display drivers.Displayer, font *Font, x int16, y int16, ch
 }
 
 // WriteLine writes a string in the selected font in the buffer
-func WriteLine(display drivers.Displayer, font *Font, x int16, y int16, text []byte, color color.RGBA) {
+func WriteLine(display drivers.Displayer, font *Font, x int16, y int16, text []rune, color color.RGBA) {
 	WriteLineRotated(display, font, x, y, text, color, 0)
 }
 
 // WriteLineRotated writes a string in the selected font in the buffer
-func WriteLineRotated(display drivers.Displayer, font *Font, x int16, y int16, text []byte, color color.RGBA, rotation Rotation) {
+func WriteLineRotated(display drivers.Displayer, font *Font, x int16, y int16, text []rune, color color.RGBA, rotation Rotation) {
 	rotation = rotation % 4
 	w, h := display.Size()
 	l := len(text)
 	for i := 0; i < l; i++ {
-		glyph := font.Glyphs[text[i]-font.First]
+		glyph := font.Glyphs[byte(text[i])-font.First]
 		//if x+int16(glyph.XAdvance) >= 0 {
 		DrawCharRotated(display, font, x, y, text[i], color, rotation)
 		//}
@@ -108,13 +109,13 @@ func WriteLineRotated(display drivers.Displayer, font *Font, x int16, y int16, t
 
 // WriteLineColors writes a string in the selected font in the buffer. Each char is in a different color
 // if not enough colors are defined, colors are cycled.
-func WriteLineColors(display drivers.Displayer, font *Font, x int16, y int16, text []byte, colors []color.RGBA) {
+func WriteLineColors(display drivers.Displayer, font *Font, x int16, y int16, text []rune, colors []color.RGBA) {
 	WriteLineColorsRotated(display, font, x, y, text, colors, 0)
 }
 
 // WriteLineColorsRotated writes a string in the selected font in the buffer. Each char is in a different color
 // if not enough colors are defined, colors are cycled.
-func WriteLineColorsRotated(display drivers.Displayer, font *Font, x int16, y int16, text []byte, colors []color.RGBA, rotation Rotation) {
+func WriteLineColorsRotated(display drivers.Displayer, font *Font, x int16, y int16, text []rune, colors []color.RGBA, rotation Rotation) {
 	numColors := uint16(len(colors))
 	if numColors == 0 {
 		return
@@ -125,7 +126,7 @@ func WriteLineColorsRotated(display drivers.Displayer, font *Font, x int16, y in
 	w, h := display.Size()
 	l := len(text)
 	for i := 0; i < l; i++ {
-		glyph := font.Glyphs[text[i]-font.First]
+		glyph := font.Glyphs[byte(text[i])-font.First]
 		//if x+int16(glyph.XAdvance) >= 0 {
 		DrawCharRotated(display, font, x, y, text[i], colors[c], rotation)
 		//}
@@ -153,18 +154,18 @@ func WriteLineColorsRotated(display drivers.Displayer, font *Font, x int16, y in
 	}
 }
 
-func LineWidth(font *Font, text []byte) (innerWidth uint32, outboxWidth uint32) {
+func LineWidth(font *Font, text []rune) (innerWidth uint32, outboxWidth uint32) {
 	for i := range text {
-		glyph := font.Glyphs[text[i]-font.First]
+		glyph := font.Glyphs[byte(text[i])-font.First]
 		outboxWidth += uint32(glyph.XAdvance)
 	}
 	innerWidth = outboxWidth
 	// first glyph
-	glyph := font.Glyphs[text[0]-font.First]
+	glyph := font.Glyphs[byte(text[0])-font.First]
 	innerWidth -= uint32(glyph.XOffset)
 
 	// last glyph
-	glyph = font.Glyphs[text[0]-font.First]
+	glyph = font.Glyphs[byte(text[0])-font.First]
 	innerWidth += -uint32(glyph.XAdvance) + uint32(glyph.XOffset) + uint32(glyph.Width)
 	return
 }
