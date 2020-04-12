465e9c6c60bd1407b79105969fada3724ac6795b
sago35 <sago35@gmail.com>
2020-04-12 10:55:32 +0900

    Change to built-in flag package

--- cmd/tinyfontgen/cli.go
+++ cmd/tinyfontgen/cli.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 const (
@@ -20,68 +19,78 @@ type cli struct {
 }
 
 var (
-	app = kingpin.New(appName, appDescription)
+	fs       = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	pkgname  = fs.String("package", "main", "package name")
+	fontname = fs.String("fontname", "TinyFont", "font name")
+	str      = fs.String("string", "", "strings for font")
+	output   = fs.String("output", "", "output path")
+	all      = fs.Bool("all", false, "include all glyphs in the font")
+	verbose  = fs.Bool("verbose", false, "run verbosely")
+	yadvance = fs.Int("yadvance", 0, "new line distance")
 )
+
+type strslice struct {
+	s []string
+}
+
+func (s *strslice) String() string {
+	return fmt.Sprintf("%v", s.s)
+}
+
+func (s *strslice) Set(v string) error {
+	s.s = append(s.s, v)
+	return nil
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: tinyfontgen [flags] [path ...]\n")
+	fs.PrintDefaults()
+}
 
 // Run ...
 func (c *cli) Run(args []string) error {
-	app.UsageWriter(c.errStream)
+	var strfiles strslice
+	var fonts []string
 
-	if VERSION != "" {
-		app.Version(fmt.Sprintf("%s version %s build %s", appName, VERSION, BUILDDATE))
-	} else {
-		app.Version(fmt.Sprintf("%s version - build -", appName))
+	fs.Var(&strfiles, "string-file", "strings file for font (can be set multiple times)")
+
+	fs.Usage = usage
+	fs.Parse(os.Args[1:])
+	for 0 < fs.NArg() {
+		fonts = append(fonts, fs.Arg(0))
+		fs.Parse(fs.Args()[1:])
 	}
-	app.HelpFlag.Short('h')
 
-	pkgname := app.Flag("package", "package name").Default("main").String()
-	fontname := app.Flag("fontname", "font name").Default("TinyFont").String()
-	fonts := app.Arg("font", "font path (*.bdf)").Required().ExistingFiles()
-	str := app.Flag(`string`, `strings for font`).String()
-	strfiles := app.Flag(`string-file`, `strings for font`).ExistingFiles()
-	output := app.Flag("output", "output path").String()
-	all := app.Flag("all", "include all glyphs in the font").Bool()
-	verbose := app.Flag("verbose", "run verbosely").Bool()
-	yadvance := app.Flag("yadvance", "new line distance").Int()
+	f := fontgen{
+		pkgname:  *pkgname,
+		fontname: *fontname,
+		fonts:    fonts,
+	}
 
-	k, err := app.Parse(args[1:])
+	w, err := os.Create(*output)
 	if err != nil {
-		return err
+		return nil
+	}
+	defer w.Close()
+
+	opts := []option{
+		withAll(*all),
+		withVerbose(*verbose),
+		withYAdvance(*yadvance),
 	}
 
-	switch k {
-	default:
-		f := fontgen{
-			pkgname:  *pkgname,
-			fontname: *fontname,
-			fonts:    *fonts,
-		}
-
-		w, err := os.Create(*output)
-		if err != nil {
-			return nil
-		}
-		defer w.Close()
-
-		opts := []option{
-			withAll(*all),
-			withVerbose(*verbose),
-			withYAdvance(*yadvance),
-		}
-
-		runes := []rune(*str)
-		for _, f := range *strfiles {
-			b, err := ioutil.ReadFile(f)
-			if err != nil {
-				return err
-			}
-			runes = append(runes, []rune(string(b))...)
-		}
-
-		err = f.generate(w, runes, opts...)
+	runes := []rune(*str)
+	for _, f := range strfiles.s {
+		b, err := ioutil.ReadFile(f)
 		if err != nil {
 			return err
 		}
+		runes = append(runes, []rune(string(b))...)
+	}
+
+	err = f.generate(w, runes, opts...)
+	if err != nil {
+		return err
 	}
 
 	return nil
