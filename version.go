96dedb4e602b1106077f85f40efed2357dc40e7c
Ron Evans <ron@hybridgroup.com>
2020-04-13 19:35:25 +0200

    all: add Version to package (#8)
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ version.go
@@ -0,0 +1,7 @@
+package tinyfont
+
+// Version returns a user-readable string showing the version of the tinyfont package
+// for support purposes.
+//
+// Update this value before release of new version of software.
+const Version = "0.2.0"
