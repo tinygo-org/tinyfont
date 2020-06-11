c2edbdfb2742e22b30ae81a4711efc9728dae6eb
sago35 <sago35@gmail.com>
2020-06-11 14:41:12 +0900

    Merge pybadge and pyportal examples, and add wioterminal example (#12)
    * Merge pybadge and pyportal examples
    
    * Add example of wioterminal
    
    * Rename pybadge example

--- examples/displays/main.go
+++ examples/displays/main.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"image/color"
-	"machine"
 
-	"tinygo.org/x/drivers/ili9341"
-	"tinygo.org/x/tinyfont"
 	"tinygo.org/x/tinyfont/freemono"
 	"tinygo.org/x/tinyfont/freesans"
 	"tinygo.org/x/tinyfont/freeserif"
 	"tinygo.org/x/tinyfont/gophers"
+
+	"tinygo.org/x/tinyfont"
 )
 
 func main() {
-	display := ili9341.NewParallel(
-		machine.LCD_DATA0,
-		machine.TFT_WR,
-		machine.TFT_DC,
-		machine.TFT_CS,
-		machine.TFT_RESET,
-		machine.TFT_RD,
-	)
-	display.Configure(ili9341.Config{})
-	display.FillScreen(color.RGBA{255, 255, 255, 255})
-
-	machine.TFT_BACKLIGHT.Configure(machine.PinConfig{machine.PinOutput})
-	machine.TFT_BACKLIGHT.High()
+	display := initDisplay()
 
 	mycolors := make([]color.RGBA, 20)
 	for k := 0; k < 20; k++ {
@@ -37,6 +24,7 @@ func main() {
 	tinyfont.WriteLineColorsRotated(display, &tinyfont.Org01, 10, 40, "TinyGo", mycolors[18:], tinyfont.ROTATION_90)
 
 	tinyfont.WriteLineColors(display, &gophers.Regular58pt, 18, 90, "ABC", mycolors[9:])
+
 }
 
 func getRainbowRGB(i uint8) color.RGBA {
---
+++ examples/displays/pyportal.go
@@ -0,0 +1,29 @@
+// +build pyportal
+
+package main
+
+import (
+	"image/color"
+	"machine"
+
+	"tinygo.org/x/drivers"
+	"tinygo.org/x/drivers/ili9341"
+)
+
+func initDisplay() drivers.Displayer {
+	d := ili9341.NewParallel(
+		machine.LCD_DATA0,
+		machine.TFT_WR,
+		machine.TFT_DC,
+		machine.TFT_CS,
+		machine.TFT_RESET,
+		machine.TFT_RD,
+	)
+	d.Configure(ili9341.Config{})
+	d.FillScreen(color.RGBA{255, 255, 255, 255})
+
+	machine.TFT_BACKLIGHT.Configure(machine.PinConfig{machine.PinOutput})
+	machine.TFT_BACKLIGHT.High()
+
+	return d
+}
