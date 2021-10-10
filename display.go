8ed48c38c1b56d9da2dadc3c455629d17fd2d1d3
sago35 <sago35@gmail.com>
2021-10-10 12:40:34 +0900

    Refactor rotation process

---
+++ display.go
@@ -0,0 +1,52 @@
+package tinyfont
+
+import (
+	"image/color"
+
+	"tinygo.org/x/drivers"
+)
+
+type RotatedDisplay struct {
+	display  drivers.Displayer
+	Rotation Rotation
+	X        int16
+	Y        int16
+}
+
+func NewRotatedDisplay(display drivers.Displayer, rotation Rotation, x, y int16) *RotatedDisplay {
+	return &RotatedDisplay{
+		display:  display,
+		Rotation: rotation,
+		X:        x,
+		Y:        y,
+	}
+}
+
+func (d *RotatedDisplay) Size() (int16, int16) {
+	x, y := d.display.Size()
+	if d.Rotation == NO_ROTATION {
+		return x, y
+	} else if d.Rotation == ROTATION_90 {
+		return y, x
+	} else if d.Rotation == ROTATION_180 {
+		return x, y
+	} else {
+		return y, x
+	}
+}
+
+func (d *RotatedDisplay) SetPixel(x, y int16, c color.RGBA) {
+	if d.Rotation == NO_ROTATION {
+		d.display.SetPixel(d.X+x, d.Y+y, c)
+	} else if d.Rotation == ROTATION_90 {
+		d.display.SetPixel(d.X-y, d.Y+x, c)
+	} else if d.Rotation == ROTATION_180 {
+		d.display.SetPixel(d.X-x, d.Y-y, c)
+	} else {
+		d.display.SetPixel(d.X+y, d.Y-x, c)
+	}
+}
+
+func (d *RotatedDisplay) Display() error {
+	return d.display.Display()
+}
