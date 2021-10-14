c2aca8f63ec7edf9e1db7fde880be9b22ae276a2
sago35 <sago35@gmail.com>
2021-10-15 08:37:59 +0900

    !!

--- examples/displays/main.go
+++ examples/displays/main.go
@@ -20,12 +20,29 @@ func main() {
 	for k := 0; k < 20; k++ {
 		mycolors[k] = getRainbowRGB(uint8(k * 14))
 	}
-	tinyfont.WriteLineColors(display, &freesans.Bold18pt7b, 10, 35, "HELLO", mycolors)
-	tinyfont.WriteLineColorsRotated(display, &freemono.Bold18pt7b, 150, 100, "Gophers", mycolors[6:], tinyfont.ROTATION_180)
-	tinyfont.WriteLineColorsRotated(display, &freeserif.Bold9pt7b, 150, 90, "TinyGo", mycolors[12:], tinyfont.ROTATION_270)
-	tinyfont.WriteLineColorsRotated(display, &tinyfont.Org01, 10, 40, "TinyGo", mycolors[18:], tinyfont.ROTATION_90)
 
-	tinyfont.WriteLineColors(display, &gophers.Regular58pt, 18, 90, "ABC", mycolors[9:])
+	//tinyfont.WriteLineColorsRotated(display, &tinyfont.NotoSans12pt, 0, 200, "!AB", mycolors, tinyfont.NO_ROTATION)
+	tinyfont.WriteLineColorsRotated(display, &tinyfont.NotoSans12pt, 5, 200, "こんにちは hello 世界", mycolors, tinyfont.NO_ROTATION)
+	tinyfont.WriteLineColorsRotated(display, &tinyfont.NotoSans12pt, 5, 220, "日本語英語English", mycolors, tinyfont.NO_ROTATION)
+
+	//tinyfont.WriteLineColorsRotated(display, &tinyfont.Twemoji12pt, 240, 200, "🀄🃏🅰", mycolors, tinyfont.NO_ROTATION)
+	//tinyfont.WriteLineColorsRotated(display, &tinyfont.Twemoji12pt, 240, 200, "🀄🃏🅰", mycolors, tinyfont.ROTATION_90)
+	//tinyfont.WriteLineColorsRotated(display, &tinyfont.Twemoji12pt, 240, 200, "🀄🃏🅰", mycolors, tinyfont.ROTATION_180)
+	//tinyfont.WriteLineColorsRotated(display, &tinyfont.Twemoji12pt, 240, 200, "🀄🃏🅰", mycolors, tinyfont.ROTATION_270)
+
+	//tinyfont.WriteLineColorsRotated(display, &freesans.Bold18pt7b, 160, 120, "HELLO", mycolors, tinyfont.NO_ROTATION)
+	//tinyfont.WriteLineColorsRotated(display, &freesans.Bold18pt7b, 160, 120, "HELLO", mycolors, tinyfont.ROTATION_90)
+	//tinyfont.WriteLineColorsRotated(display, &freesans.Bold18pt7b, 160, 120, "HELLO", mycolors, tinyfont.ROTATION_180)
+	//tinyfont.WriteLineColorsRotated(display, &freesans.Bold18pt7b, 160, 120, "HELLO", mycolors, tinyfont.ROTATION_270)
+
+	if true {
+		tinyfont.WriteLineColors(display, &freesans.Bold18pt7b, 10, 35, "HELLO", mycolors)
+		tinyfont.WriteLineColorsRotated(display, &freemono.Bold18pt7b, 150, 100, "Gophers", mycolors[6:], tinyfont.ROTATION_180)
+		tinyfont.WriteLineColorsRotated(display, &freeserif.Bold9pt7b, 150, 90, "TinyGo", mycolors[12:], tinyfont.ROTATION_270)
+		tinyfont.WriteLineColorsRotated(display, &tinyfont.Org01, 10, 40, "TinyGo", mycolors[18:], tinyfont.ROTATION_90)
+
+		tinyfont.WriteLineColors(display, &gophers.Regular58pt, 18, 90, "ABC", mycolors[9:])
+	}
 
 	for {
 		time.Sleep(time.Hour)
