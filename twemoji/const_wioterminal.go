49950db378731b48f4375f5c5ffabec218e78ca0
sago35 <sago35@gmail.com>
2021-10-23 11:05:16 +0900

    Update

---
+++ twemoji/const_wioterminal.go
@@ -0,0 +1,30 @@
+//go:build baremetal
+// +build baremetal
+
+package twemoji
+
+import (
+	"tinygo.org/x/drivers/flash"
+)
+
+var (
+	dev     *flash.Device
+	fontBuf [1024]byte
+	offset  = 0x000A0000
+)
+
+func FlashDevice(d *flash.Device) {
+	dev = d
+}
+
+func (g Twemoji12ptGlyph) Bytes() []byte {
+	buf := fontBuf[:12*12*2]
+	for {
+		_, err := dev.ReadAt(buf, int64(g.Offset)+int64(offset))
+		if err != nil {
+			continue
+		}
+		break
+	}
+	return buf
+}
